c3cd697adcf2b069af998ae754c748f64b55ea1f
ShubhamMewara <shubhammewara12@gmail.com>
2025-01-07 17:28:43 +0530

    fix file attachment

--- lib/preprocess.ts
+++ lib/preprocess.ts
@@ -2,13 +2,6 @@
 export async function preProcessFile(file: File) {
   const parsed = await file.text();
 
-  // get first 5 lines of csv
-  if (file.type === "text/csv") {
-    const lines = parsed.split("\n");
-    const content = lines.slice(0, 5).join("\n");
-    return content;
-  }
-
   // return the raw content
   return parsed;
 }
